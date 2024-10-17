4035388c83aa52ae30ccce29c321a10a251ce2ab
Lara Ehart <1033@htl.rennweg.at>
2024-10-17 14:48:56 +0200

    User Story 3 GET parameter für speziellen OSR

--- webt-core-server-responses-with-php-in-json-format/src/Demo.php
+++ webt-core-server-responses-with-php-in-json-format/src/Demo.php
@@ -2,18 +2,34 @@
 require 'OST.php';
 require 'Song.php';
 require 'Seeder.php';
-class Demo {
-    public static function run() {
-        
-        $OSTs = Seeder::seed();
 
-        $OSTArrays = [];
-        foreach ($OSTs as $OST) {
-            print_r($OST);
+header('Content-Type: application/json');   
+
+$OSTs = Seeder::seed();
+
+/*
+foreach ($OSTs as $OST) {
+    print_r($OST);
+}
+*/
+
+function getOSTByID($id, $OSTList) {
+    foreach ($OSTList as $OST) {
+        if ($OST->id == $id) {
+            return $OST;
         }
+    }
+    return null;
+}
 
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $OST = getOSTByID($id, $OSTs);
+
+    if ($OST) {
+        echo json_encode($OST);
     }
 }
 
-Demo::run();
+
 ?>
