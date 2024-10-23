86dc448d231bc5b37dd259cc90b0c638a9272d73
Lara Ehart <1033@htl.rennweg.at>
2024-10-23 11:59:13 +0200

    verbesserungen durchführen für server responses with php in json

--- webt-core-server-responses-with-php-in-json-format/public/Demo.php
+++ webt-core-server-responses-with-php-in-json-format/public/Demo.php
@@ -1,7 +1,7 @@
 <?php
-require 'OST.php';
-require 'Song.php';
-require 'Seeder.php';
+require '../src/OST.php';
+require '../src/Song.php';
+require '../src/Seeder.php';
 
 header('Content-Type: application/json');   
 
@@ -15,12 +15,13 @@ foreach ($OSTs as $OST) {
 
 function getOSTByID($id, $OSTList) {
     foreach ($OSTList as $OST) {
-        if ($OST->id == $id) {
+        if ($OST->getId() == $id) {
             return $OST;
         }
     }
     return null;
 }
+
 
 if (isset($_GET['id'])) {
     $id = intval($_GET['id']);      //intval() konvertiert den GET-Parameter in ein int, sodass z.B. wenn man einen String angibt nur 0 zurückkommt und nicht etwas schädliches ausgeführt werden kann.
--- webt-core-server-responses-with-php-in-json-format/src/OST.php
+++ webt-core-server-responses-with-php-in-json-format/src/OST.php
@@ -1,13 +1,8 @@
 <?php
-    class OST {
-        public $id;
-        public $name;
-        public $gameName;
-        public $releaseYear;
-        public $trackList = [];
+    class OST implements JsonSerializable {
 
         //Constructor 
-        public function __construct($id, $name, $gameName, $releaseYear, $trackList = []) {
+        public function __construct(protected int $id, protected string $name, protected string $gameName, protected int $releaseYear, protected array $trackList) {
             $this->id = $id;
             $this->name = $name;
             $this->gameName = $gameName;
@@ -15,9 +10,34 @@
             $this->trackList = $trackList;
         }
 
-        
-        public function addSong(Song $song) {
-            $this->trackList[] = $song;    //Song wird einfach am Ende zum Array dazugefügt
+        public function getId(): int {
+            return $this->id;
+        }
+
+        public function getName(): string {
+            return $this->name;
+        }
+
+        public function getGameName(): string {
+            return $this->gameName;
+        }
+
+        public function getReleaseYear(): int {
+            return $this->releaseYear;
+        }
+
+        public function getTrackList(): array {
+            return $this->trackList;
+        }
+
+        public function jsonSerialize(): array {
+            return [
+                'id' => $this->getId(),
+                'name' => $this->getName(),
+                'gameName' => $this->getGameName(),
+                'releaseYear' => $this->getReleaseYear(),
+                'trackList' => $this->getTrackList(),
+            ];
         }
 
     }
