db5fac79ac80801eaa33051deaf050314afc5368
Lara Ehart <1033@htl.rennweg.at>
2024-10-23 12:26:50 +0200

    verbesserungen

--- webt-core-server-responses-with-php-in-json-format/public/Demo.php
+++ webt-core-server-responses-with-php-in-json-format/public/Demo.php
@@ -7,7 +7,7 @@ header('Content-Type: application/json');
 
 $OSTs = Seeder::seed();
 
-//funktion um nur einen OST mit einer spezifischen id zu bekommen
+//Funktion um nur einen OST mit einer spezifischen id zu bekommen
 function getOSTByID($id, $OSTList) {
     foreach ($OSTList as $OST) {
         if ($OST->getId() == $id) {
@@ -17,17 +17,22 @@ function getOSTByID($id, $OSTList) {
     return null;
 }
 
-if (isset($_GET['id']) && isset($_GET['all']) && $_GET['all'] == true) {
+if (isset($_GET['id']) && isset($_GET['all']) && $_GET['all'] === 'true') {
     echo json_encode($OSTs);
 } else if (isset($_GET['id'])) {
     $id = intval($_GET['id']);      //intval() konvertiert den GET-Parameter in ein int, sodass z.B. wenn man einen String angibt nur 0 zurückkommt und nicht etwas schädliches ausgeführt werden kann.
+    if (count($OSTs) < $id){
+        echo "zu großer Wert";
+    }
     $OST = getOSTByID($id, $OSTs);
 
     if ($OST) {
         echo json_encode($OST);
     }
-} else if (isset($_GET['all']) && $_GET['all'] == true) {
+} else if (isset($_GET['all']) && $_GET['all'] === 'true') {
     echo json_encode($OSTs);
-} 
+} else if (isset($_GET['all']) && $_GET['all'] !== 'true') {
+    echo "falsche Eingabe";
+}
 
 ?>
