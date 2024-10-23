60276fa70b473d6577c6cfb73fbea7f11523e226
Lara Ehart <1033@htl.rennweg.at>
2024-10-23 12:01:36 +0200

    kommentar löschen

--- webt-core-server-responses-with-php-in-json-format/public/Demo.php
+++ webt-core-server-responses-with-php-in-json-format/public/Demo.php
@@ -7,11 +7,6 @@ header('Content-Type: application/json');
 
 $OSTs = Seeder::seed();
 
-/*
-foreach ($OSTs as $OST) {
-    print_r($OST);
-}
-*/
 
 function getOSTByID($id, $OSTList) {
     foreach ($OSTList as $OST) {
