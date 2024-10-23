cf56c94b07200abba68d183f65ed4474293c2fb4
Lara Ehart <1033@htl.rennweg.at>
2024-10-23 12:07:09 +0200

    if statements anpassen

--- webt-core-server-responses-with-php-in-json-format/public/Demo.php
+++ webt-core-server-responses-with-php-in-json-format/public/Demo.php
@@ -18,18 +18,19 @@ function getOSTByID($id, $OSTList) {
 }
 
 
-if (isset($_GET['id'])) {
+if (isset($_GET['id']) && isset($_GET['all']) && $_GET['all'] == true) {
+    echo json_encode($OSTs);
+} else if (isset($_GET['id'])) {
     $id = intval($_GET['id']);      //intval() konvertiert den GET-Parameter in ein int, sodass z.B. wenn man einen String angibt nur 0 zurückkommt und nicht etwas schädliches ausgeführt werden kann.
     $OST = getOSTByID($id, $OSTs);
 
     if ($OST) {
         echo json_encode($OST);
     }
-}
-
-
-if (isset($_GET['all']) && $_GET['all'] == true) {
+} else if (isset($_GET['all']) && $_GET['all'] == true) {
     echo json_encode($OSTs);
-}
+} 
+
+
 
 ?>
