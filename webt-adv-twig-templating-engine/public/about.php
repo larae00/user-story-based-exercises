12c2039ab6bb637a19c467b157f6fd67193f2a12
Lara Ehart <1033@htl.rennweg.at>
2025-02-26 10:46:07 +0100

    implements user story 1 & 2 - twig templating engine

---
+++ webt-adv-twig-templating-engine/public/about.php
@@ -0,0 +1,21 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader('../src');
+$twig = new \Twig\Environment($loader);
+$url = 'webt-adv-twig-templating-engine/public';
+
+echo $twig->render('index.html.twig', [
+    'title' => 'Meine Webseite',
+    'headline' => 'Willkommen auf meiner Seite',
+    'name' => 'Lara',
+    'content' => 'Dies ist der Hauptinhalt der Seite.',
+    'date' => '2025',
+    'ich' => 'Lara Ehart',
+    'navigation' => [
+        ['url' => './', 'text' => 'Home'],
+        ['url' => './about.php', 'text' => 'Über mich'],
+        ['url' => './contact.php', 'text' => 'Kontakt']
+    ]
+]);
+?>
